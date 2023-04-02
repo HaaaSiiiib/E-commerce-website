34524093cde4172b37d13fd2bf31d0956aa1a7df
Fuad Al Hasib <fuad36@student.sust.edu>
2023-04-03 05:43:53 +0600

    changed the old name

--- CSE-446-backend/bank/db/knex.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({ path: '../.env' });
-const knex = require('knex');
-const knexfile = require('./knexfile');
-
-module.exports = knex(knexfile[process.env.DEV_MODE]);
--- CSE-446-backend/supplier/api/v1/routes/supplies.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supplyController = require('../controllers/supplies');
-//const isAuth = require('../middlewares/is-auth');
-
-router.post('/supplies', supplyController.postSupply);
-
-module.exports = router;
